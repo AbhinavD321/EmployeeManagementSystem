6ad2d534721f50689c0c339e2901df05b8db5bfa
AbhinavD321 <abjbn@gmail.com>
2026-06-22 08:31:16 +0530

    Read debug from environment

--- ems/settings.py
+++ ems/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+i_^capwmuejll!gbd2+$&jk$gy0dc_md1w3x!!&^*eb@qs!6*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['*']
 
